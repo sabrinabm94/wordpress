22921938c6d1bb819bdde3c81b06e133606ce991
Sabrina B <sabrinabgbc@email.com>
2018-08-30 16:41:52 -0300

    Update functions.php
    adicionada função de adicionar classes ao body das páginas

--- wp+docker/sabrina/functions.php
+++ wp+docker/sabrina/functions.php
@@ -3,6 +3,8 @@ add_filter('post_thumbnail_html', 'wpb_autolink_featured_images', 10, 3);
 add_action('template_redirect', 'enqueue');
 add_action('widgets_init', 'register_my_sidebars');
 add_action('init', 'register_menus');
+add_filter('body_class', 'add_class_on_body');
+
 function enqueue() {
 	wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/css/boostrap.min.css');
 	wp_enqueue_style('bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css');
@@ -14,20 +16,22 @@ function enqueue() {
 	wp_enqueue_script('boostrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js', array('jquery'));
 
 }
-function wpb_autolink_featured_images( $html, $post_id, $post_image_id ) {
-	if (! is_singular()) { 
-		$html = '<a href="' . get_permalink( $post_id ) . '" title="' . esc_attr( get_the_title( $post_id ) ) . '">' . $html . '</a>';
+
+function wpb_autolink_featured_images($html, $post_id, $post_image_id) {
+	if (!is_singular()) { 
+		$html = '<a href="' . get_permalink($post_id) . '" title="' . esc_attr(get_the_title($post_id)) . '">' . $html . '</a>';
 		return $html;
 	} else { 
 		return $html;
 	}
 }
+
 function register_my_sidebars() {
     register_sidebar(
         array(
 	        'id' => 'sidebar_page',
-	        'name' => __( 'Sidebar Page' ),
-	        'description' => __( 'A short description of the sidebar.' ),
+	        'name' => __('Sidebar Page'),
+	        'description' => __('A short description of the sidebar.'),
 	        'before_widget' => '<div id="%1$s" class="widget %2$s">',
 	        'after_widget' => '</div>',
 	        'before_title' => '<h3 class="widget-title">',
@@ -37,8 +41,8 @@ function register_my_sidebars() {
     register_sidebar(
         array(
 	        'id' => 'sidebar_footer',
-	        'name' => __( 'Sidebar Footer' ),
-	        'description' => __( 'A short description of the sidebar.' ),
+	        'name' => __('Sidebar Footer'),
+	        'description' => __('A short description of the sidebar.'),
 	        'before_widget' => '<div id="%1$s" class="widget %2$s">',
 	        'after_widget' => '</div>',
 	        'before_title' => '<h3 class="widget-title">',
@@ -46,6 +50,7 @@ function register_my_sidebars() {
     	)
     );
 }
+
 function register_menus() {
 	register_nav_menus (
 		array(
@@ -53,7 +58,16 @@ function register_menus() {
 		)
 	);
 }
+
 if (has_post_thumbnail()) {
 	the_post_thumbnail();
+}
+
+function add_class_on_body($classes) {
+	global $post;
+	if (isset( $post)) {
+		$classes[] = $post->post_type . '-' . $post->post_name;
+	}
+	return $classes;
 }
 ?>
